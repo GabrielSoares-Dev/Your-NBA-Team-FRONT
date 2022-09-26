3b9febdabcdfb5d4331c332debd377f66370ccf7
Gabriel <gsm2015@outlook.com.br>
2022-09-25 22:24:55 -0300

    inicializando login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,32 @@
 import { useAppSelector, useAppDispatch } from '@store/hooks';
 import { setSun } from '@store/modules/Auth/index.store';
 import Layout from '@components/Layouts/Layout';
-import { TextField, Grid, Paper, Box } from '@mui/material';
+import {
+ TextField, Grid, Paper, Box
+} from '@mui/material';
 import type { NextPage } from 'next';
 
 const Login: NextPage = () => {
   const { payload } = useAppSelector(state => state.auth);
   const dispatch = useAppDispatch();
-  console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID);
   return (
     <Layout>
       <Box justifyContent="center" display="flex" marginTop={20}>
-        <Paper style={{ height: 200, width: '300px' }}>
+        <Paper elevation={5} style={{ height: 200, width: '300px' }}>
           <Grid container flexDirection="column" alignItems="center">
             <Grid item>
               <TextField
                 color="primary"
                 placeholder="email@outlook.com.br"
                 variant="outlined"
+                label="Email"
+                helperText="test"
               />
             </Grid>
             <Grid item>
               <TextField
                 color="primary"
-                placeholder="email@outlook.com.br"
+                placeholder="senha"
                 variant="outlined"
               />
             </Grid>
